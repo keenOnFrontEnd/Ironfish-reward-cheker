8020b6c5541c798e2fd101a5e5b3d686d82e7568
keenOnFrontEnd <73851144+keenOnFrontEnd@users.noreply.github.com>
2022-05-21 15:59:11 +0200

    Creating app

---
+++ ironfish/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import IndexComponent from "./main";
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+  <IndexComponent />
+    </div>
+
+  );
+}
+
+export default App;
---
+++ ironfish/src/main/index.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+const IndexComponent = () => {
+  
+  const []
+    return(
+      <div>
+
+      </div>
+  )
+};
+
+export default IndexComponent;
