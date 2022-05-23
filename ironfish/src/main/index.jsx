db2a9585eb56b7098db99f5a05455cb0dd709a50
keenOnFrontEnd <73851144+keenOnFrontEnd@users.noreply.github.com>
2022-05-23 14:45:36 +0200

    axios adding + trying to fetch data from server

--- ironfish/src/main/index.jsx
+++ ironfish/src/main/index.jsx
@@ -1,13 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const IndexComponent = () => {
-  
-  const []
-    return(
-      <div>
+  // const [isLoading, setIsLoading] = useState()
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        await fetch(
+          `https://cors-anywhere.herokuapp.com/api.ironfish.network/users?order_by=rank&limit=100`,
+          {
+            headers: {
+              'origin': 'http://localhost:3000/'
+            }
+          }
+        )
+          .then((responce) => responce.json())
+          .then((data) => console.log(data));
+        // console.log(res);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    fetchData();
+  }, []);
 
-      </div>
-  )
+  //  console.log(isLoading)
+
+  return <div>test</div>;
 };
 
 export default IndexComponent;
