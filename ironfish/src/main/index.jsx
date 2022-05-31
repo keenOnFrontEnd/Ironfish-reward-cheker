97a8defd3b0644c8c6e899ada5a30fa362faedfe
keenOnFrontEnd <73851144+keenOnFrontEnd@users.noreply.github.com>
2022-05-31 13:04:25 +0200

    reset to UI

--- ironfish/src/main/index.jsx
+++ ironfish/src/main/index.jsx
@@ -1,15 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React, {  useEffect, useState } from "react";
 import axios from "axios";
 import { FetchUserData, server_metric, test_state } from "./fetchComponent";
 import CustomUserComponent from "./customUserComponent";
-import styles from "../css/main.css";
-import LogoComponent from "./logo";
 
 const IndexComponent = () => {
+
+
   const [isLoading, setIsLoading] = useState(true);
   const [userState, setUserState] = useState([]);
-  const [userMetrics, setUserMetrics] = useState([]);
-  const [userMoniker, setUserMoniker] = useState();
+  const [userMetrics,setUserMetrics] = useState([]);
+  const [userMoniker, setUserMoniker] = useState()
 
   // async function fetchData() {
   //   try {
@@ -23,38 +23,18 @@ const IndexComponent = () => {
   //   }
   // }
 
-  const search = () => {
-    FetchUserData(userMoniker, setUserState, setUserMetrics, setIsLoading);
-  };
+  const search = async () => {
+    FetchUserData(userMoniker, setUserState, setUserMetrics, setIsLoading)
+  }
+  console.log(isLoading)
+  console.log(userState)
+  console.log(userMetrics)
 
-  console.log(isLoading);
-  console.log(userState);
-  console.log(userMetrics);
-
-  return (
-    <div className="container">
-      <div className="mainText">IronFish Rewards Checker</div>
-
-      <div className="searchContainer">
-        <input
-          placeholder="To calculate rewards, put graffiti here"
-          onChange={(e) => setUserMoniker(e.target.value)}
-          type="search"
-        />
-        <button onClick={() => search()}>Search</button>
-      </div>
-
-      {isLoading === false ? (
-        <CustomUserComponent
-          userMetrics={userMetrics}
-          server_metrics={server_metric}
-          moniker={userMoniker}
-        />
-      ) : (
-        <LogoComponent className={isLoading === true ? ('') : ('logoFadeOut')} />
-      )}
-    </div>
-  );
+  return <div>
+    <input placeholder="put your graffiti here" onChange={(e) => setUserMoniker(e.target.value)} />
+    <button onClick={() => search()}>Find</button>
+  {isLoading === false ? (<CustomUserComponent userMetrics={userMetrics} server_metrics={server_metric}/>) : (<></>)}
+  </div>;
 };
 
 export default IndexComponent;
