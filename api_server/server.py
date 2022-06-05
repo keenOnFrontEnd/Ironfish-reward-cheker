92470a7d686d58d94b38af3bd3b7d0b1b18489e2
Smorchkov Misha <mishutka200101@mail.ru>
2022-06-05 20:46:08 +0300

    cors enabled

--- api_server/server.py
+++ api_server/server.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from ph2_rewards import pool1
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=origins,
+    allow_headers=origins,
+)
 
 
 @app.get("/pool1_points")
